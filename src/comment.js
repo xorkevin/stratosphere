4de15e8f54e703a5819993144445e2bbb1422dec
Kevin Wang <wangkevin448@gmail.com>
2017-08-25 13:17:01 -0700

    update comment structure

--- src/comment.js
+++ src/comment.js
@@ -4,18 +4,20 @@ import Time from 'time';
 class Comment extends React.Component {
   render(){
     return <div className="comment">
-      <div className="info">
-        <span className="username"><a>{this.props.username}</a></span>
-        <span className="data score">{this.props.score} points</span>
-        <span className="data time"><Time value={this.props.time}/></span>
-      </div>
-      <div className="content">{this.props.content}</div>
-      <div className="options">
-        <span><a className="no-color">link</a></span>
-        <span><a className="no-color">source</a></span>
-        <span><a className="no-color">reply</a></span>
-        <span><a className="no-color">hide</a></span>
-        <span><a className="no-color">report</a></span>
+      <div className="inner">
+        <div className="info">
+          <span className="username"><a>{this.props.username}</a></span>
+          <span className="data score">{this.props.score} points</span>
+          <span className="data time"><Time value={this.props.time}/></span>
+        </div>
+        <div className="content">{this.props.content}</div>
+        <div className="options">
+          <span><a className="no-color">link</a></span>
+          <span><a className="no-color">source</a></span>
+          <span><a className="no-color">reply</a></span>
+          <span><a className="no-color">hide</a></span>
+          <span><a className="no-color">report</a></span>
+        </div>
       </div>
       { this.props.children && <div className="children">
         { this.props.children }
--- src/main.js
+++ src/main.js
@@ -123,25 +123,28 @@ render(
     <h6>Heading 6 <small>small</small></h6>
     <span>Text <small>small</small></span>
 
-    <Article title="Lorem ipsum" author="Kevin Wang" time="2017-02-10T09:48:34Z">
+    <Article title="Lorem ipsum" author="Kevin Wang" time={Date.now()-86400000}>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla aliquet condimentum. Nunc facilisis orci dui, sit amet dictum massa porta at. Mauris augue nisi, scelerisque ac suscipit sit amet, egestas ut risus. In hac habitasse platea dictumst. Vivamus nibh enim, dignissim quis consequat at, sagittis in magna. Aliquam accumsan, nisl vel sollicitudin fringilla, libero neque vehicula mauris, eu laoreet nunc ligula convallis nulla. Aliquam felis elit, fermentum ac felis sagittis, porttitor placerat odio. Ut consectetur est lectus, sed maximus libero malesuada ut. Proin aliquet, sapien et pretium feugiat, dui diam posuere diam, ut tempor elit purus quis metus.</p>
       <p>Nulla facilisi. Phasellus blandit interdum est, in pellentesque nunc fermentum et. Proin nibh risus, sollicitudin ac urna sed, aliquet hendrerit massa. Pellentesque vehicula fringilla purus, sit amet bibendum turpis malesuada in. Aliquam nisl enim, elementum id dapibus at, suscipit non arcu. Suspendisse sodales massa vitae dolor vestibulum, lacinia congue enim hendrerit. Curabitur dapibus, arcu a pulvinar pulvinar, lectus elit eleifend dolor, id tincidunt nunc dolor eu orci. Sed neque massa, cursus et enim quis, gravida fermentum est. Nam non justo accumsan arcu volutpat ullamcorper sit amet nec mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer a sagittis nibh, sit amet posuere sapien. Aliquam erat volutpat. Phasellus vitae cursus turpis, posuere viverra diam. Fusce mollis consectetur ligula.</p>
     </Article>
     <CommentSection>
-      <Comment username="xorkevin" score={256} time="2017-02-10T10:34:08Z" 
+      <Comment username="xorkevin" score={256} time={Date.now()-0.5*86400000}
         content="Lorem ipsum dolor sit amet">
-        <Comment username="xorkevin" score={32} time="2017-02-10T11:58:07Z" 
+        <Comment username="xorkevin" score={32} time={Date.now()-0.25*86400000}
           content="Consectetur adipiscing elit">
-          <Comment username="xorkevin" score={16} time="2017-02-11T00:15:38Z" 
-            content="Integer fringilla aliquet condimentum">
+          <Comment username="xorkevin" score={16} time={Date.now()}
+            content="Mauris augue nisi, scelerisque ac suscipit sit amet, egestas ut risus">
           </Comment>
         </Comment>
-        <Comment username="xorkevin" score={128} time="2017-02-10T10:42:03Z" 
+        <Comment username="xorkevin" score={128} time={Date.now()-0.325*86400000}
           content="Nunc facilisis orci dui, sit amet dictum massa porta at">
         </Comment>
       </Comment>
-      <Comment username="xorkevin" score={64} time="2017-02-10T11:27:42Z" 
-        content="Mauris augue nisi, scelerisque ac suscipit sit amet, egestas ut risus">
+      <Comment username="xorkevin" score={64} time={Date.now()-0.75*86400000}
+        content="Integer fringilla aliquet condimentum">
+        <Comment username="xorkevin" score={8} time={Date.now()-0.015625*86400000}
+          content="In hac habitasse platea dictumst">
+        </Comment>
       </Comment>
     </CommentSection>
   </Section>
